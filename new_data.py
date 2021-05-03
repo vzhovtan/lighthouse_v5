9c4bf3195fdb0ae7a717301368b74b5527179814
Vadim Zhovtanyuk <43018612+vzhovtan@users.noreply.github.com>
2021-05-02 21:38:41 -0700

    Delete new_data.py

--- new_data.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-with open('new_data_entry_format.yml', 'r') as file:
-        data = yaml.load(file, Loader=yaml.FullLoader)
-        print(data)
-        # for key in data:
-        #     valid_line_card.append(key)
